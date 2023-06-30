0292861d84054d7de56aff2de65a3030189d32d0
sumgu <sum.dv@geekup.vn>
2023-06-30 23:11:45 +0700

    Fix bug array image in model

--- go.mod
+++ go.mod
@@ -3,10 +3,9 @@ module clothing-shop
 go 1.19
 
 require (
-	github.com/core-go/service v0.3.5
-	github.com/core-go/sql v0.3.6
-	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
+	gorm.io/driver/postgres v1.5.2
+	gorm.io/gorm v1.25.1
 )
 
 require (
@@ -15,8 +14,7 @@ require (
 	github.com/jackc/pgx/v5 v5.4.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.10.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
-	gorm.io/driver/postgres v1.5.2 // indirect
-	gorm.io/gorm v1.25.1 // indirect
 )
--- model/product_model.go
+++ model/product_model.go
@@ -1,15 +1,19 @@
 package model
 
+import (
+	pg "github.com/lib/pq"
+)
+
 type Product struct {
-	Id               *string `json:"id" gorm:"column:id;primary_key;autoIncrement"`
-	ProductCode      *string `json:"productCode" gorm:"column:productcode"`
-	CategoryParentId *int    `json:"categoryParentId" gorm:"column:categoryparentid"`
-	CategoryChildId  *int    `json:"categoryChildId" gorm:"column:categorychildid"`
-	Title            *string `json:"title" gorm:"column:title"`
-	Details          *string `json:"details" gorm:"column:details"`
-	// Image            []string `json:"image" gorm:"column:image"`
-	OldPrice       *int    `json:"oldPrice" gorm:"column:oldprice"`
-	Price          *int    `json:"price" gorm:"column:price"`
-	Size           *string `json:"size" gorm:"column:size"`
-	ProductQuality *string `json:"productQuality" gorm:"column:productquality"`
+	Id               *string        `json:"id" gorm:"column:id;primary_key;autoIncrement"`
+	ProductCode      *string        `json:"productCode" gorm:"column:productcode"`
+	CategoryParentId *int           `json:"categoryParentId" gorm:"column:categoryparentid"`
+	CategoryChildId  *int           `json:"categoryChildId" gorm:"column:categorychildid"`
+	Title            *string        `json:"title" gorm:"column:title"`
+	Details          *string        `json:"details" gorm:"column:details"`
+	Image            pg.StringArray `json:"image" gorm:"column:image;type:text[]"`
+	OldPrice         *int           `json:"oldPrice" gorm:"column:oldprice"`
+	Price            *int           `json:"price" gorm:"column:price"`
+	Size             *string        `json:"size" gorm:"column:size"`
+	ProductQuality   *string        `json:"productQuality" gorm:"column:productquality"`
 }
